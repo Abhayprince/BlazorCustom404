326b7dc46330c7c041fb65850f6a66f7a55c941d
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2024-08-30 19:53:10 +0530

    Add project files.

---
+++ BlazorCustom404/Program.cs
@@ -0,0 +1,33 @@
+using BlazorCustom404.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseStatusCodePagesWithRedirects("/");
+
+app.UseStatusCodePagesWithReExecute("/error-page/{0}");
+
+//app.UseStatusCodePages()
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
